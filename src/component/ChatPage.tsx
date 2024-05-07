c584c22762added2d9991c91012cb874d9f4fa9f
kym8821 <louie9798@naver.com>
2024-05-07 12:59:37 +0900

    저장용

--- src/component/ChatPage.tsx
+++ src/component/ChatPage.tsx
@@ -1,116 +1,35 @@
 import { CompatClient, Stomp } from "@stomp/stompjs";
 import React, { useRef, useState } from "react";
 import SockJS from "sockjs-client";
+import { SocketClient, connect, generateSocketClient } from "../util/socket";
 
-interface SocketClient {
-  username: string;
-  client: CompatClient;
-}
-
-const GenerateClient = (username: string) => {
-  return {
-    username: username,
-    client: Stomp.over(() => {
-      const sock = new SockJS("http://localhost:8080/ws");
-      return sock;
-    }),
-  } as SocketClient;
-};
-
-const MainPage = () => {
+const ChatPage = () => {
+  const [socketClient, setSocketClient] = useState<SocketClient>();
+  const [userId, setUserId] = useState<number>(-1);
   const inputRef = useRef(null);
-  const [message, setMessage]: any = useState(undefined);
-  const [messageContent, setMessageContent]: any = useState("hello");
-  const [socketClients, setSocketClients]: any = useState([GenerateClient("kym8821"), GenerateClient("louie9798")]);
-
-  const connectSocket = (socketClient: SocketClient) => {
-    socketClient.client.connect(
-      { user: socketClient.username },
-      () => {
-        // callback 함수 설정, 대부분 여기에 sub 함수 씀
-        console.log("connect success!");
-        socketClient.client?.subscribe(`/topic/notice`, (message) => {
-          console.log(message);
-          setMessage(message.body);
-        });
-        socketClient.client?.subscribe(`/user/queue/sendToUser`, (message) => {
-          console.log(message);
-          setMessage(message.body);
-        });
-        socketClient.client?.subscribe(`/user/${socketClient.username}/queue/sendToUser`, (message) => {
-          console.log(message);
-          setMessage(message.body);
-        });
-      },
-      () => {
-        console.log("connection failed...");
-      },
-      () => {
-        console.log("connection closed");
-      }
-    );
-  };
-
-  const connectHandler = () => {
-    socketClients.map(async (socketClient: SocketClient) => {
-      console.log("connect " + socketClient.username);
-      await connectSocket(socketClient);
-    });
-  };
-
-  const sendHandler = () => {
-    socketClients.map((socketClient: SocketClient) => {
-      if (!socketClient.client) return;
-      console.log(socketClient.username);
-      socketClient.client.send(
-        "/app/notice",
-        {
-          user: socketClient.username,
-        },
-        JSON.stringify({
-          message: "send to everybody",
-        })
-      );
-    });
-  };
-
-  const sendToUserHandler = () => {
-    const senderClient = socketClients[0].client;
-    if (!senderClient) return;
-    senderClient.send(
-      "/app/sendToUser",
-      {},
-      JSON.stringify({
-        message: "send to user",
-        username: "louie9798",
-      })
-    );
-  };
-
-  const disconnectHandler = () => {
-    socketClients.map(async (socketClient: SocketClient) => {
-      const client = socketClient.client;
-      if (!client) return;
-      client.disconnect();
-    });
-  };
 
   const inputHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget.value;
-    await setMessageContent(target);
-    console.log(messageContent);
+    if (isNaN(parseInt(target))) return;
+    await setUserId(parseInt(target));
+  };
+
+  const connectHandler = async () => {
+    console.log(userId);
+    if (isNaN(userId) || userId < 0) {
+      alert("userId is not number");
+      return;
+    }
+    const client = connect(generateSocketClient(userId));
+    await setSocketClient(client);
   };
 
   return (
     <div>
-      <div>{message}</div>
-      <input type="text" ref={inputRef} onChange={inputHandler} />
+      <input type="text" onChange={inputHandler} ref={inputRef} />
       <button onClick={connectHandler}>connect</button>
-      <button onClick={sendHandler}>submit</button>
-      <button onClick={sendToUserHandler}>submitToUser</button>
-      <button onClick={disconnectHandler}>disconnect</button>
     </div>
   );
 };
 
-export default MainPage;
+export default ChatPage;
--- src/util/socket.ts
+++ src/util/socket.ts
@@ -1,7 +1,7 @@
 import { CompatClient, Stomp } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 
-interface SocketClient {
+export interface SocketClient {
   userId: Number;
   client: CompatClient;
 }
@@ -14,4 +14,34 @@ export const generateSocketClient = (userId: Number) => {
       return sock;
     }),
   } as SocketClient;
+};
+
+function socketErrorHandler() {
+  console.log("connection failed...");
+}
+
+function socketColsureHandler() {
+  console.log("connection closed");
+}
+
+export const connect = (socketClient: SocketClient) => {
+  socketClient.client.connect(
+    { user: socketClient.userId },
+    () => {
+      // callback 함수 설정, 대부분 여기에 sub 함수 씀
+      console.log("connect success!");
+      socketClient.client?.subscribe(`/topic/notice`, (message) => {
+        console.log(message);
+      });
+      socketClient.client?.subscribe(`/user/queue/sendToUser`, (message) => {
+        console.log(message);
+      });
+      socketClient.client?.subscribe(`/user/${socketClient.userId}/queue/sendToUser`, (message) => {
+        console.log(message);
+      });
+    },
+    socketErrorHandler,
+    socketColsureHandler
+  );
+  return socketClient;
 };
